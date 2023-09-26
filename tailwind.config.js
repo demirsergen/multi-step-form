9e655e69a949b600214d3bb86ef29663a0b3ff92
Sergen Demir <dmrsrgn@gmail.com>
2023-09-26 09:51:25 -0500

    update tailwind.config for background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,14 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-radial':
+          'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'hero-bg':
+          "url('../../public/assets/images/bg-sidebar-mobile.svg')",
       },
     },
   },
   plugins: [],
-}
+};
